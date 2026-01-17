20ababc6fcbbf937184192fa4e220d49891e8611
bogdan3423 <rusbogdan3423@gmail.com>
2026-01-17 16:37:30 +0200

    fix: remove redundant list items for clarity in product features

--- app/lucrari-diploma/page.tsx
+++ app/lucrari-diploma/page.tsx
@@ -126,12 +126,6 @@ export default function LucrariDiplomaPage() {
                   <svg className="w-6 h-6 text-orange-500 mr-2 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
-                  <span><strong>Multiple exemplare</strong> - oferim reduceri pentru comenzi de mai multe volume</span>
-                </li>
-                <li className="flex items-start">
-                  <svg className="w-6 h-6 text-orange-500 mr-2 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                  </svg>
                   <span><strong>Verificare pre-printare</strong> - ne asigurăm că totul este perfect înainte de printare</span>
                 </li>
               </ul>
